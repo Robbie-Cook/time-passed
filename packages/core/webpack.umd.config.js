c9d7c8baf2601f350065093d7b740f64cfc907e8
Robbie Cook <robbieck7@gmail.com>
2020-11-13 20:41:53 +1300

    Adding build steps

---
+++ packages/core/webpack.umd.config.js
@@ -0,0 +1,64 @@
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/index.ts"),
+  output: {
+    path: path.resolve(__dirname, "./umd"),
+    filename: "index.js",
+    libraryTarget: "umd",
+    library: "MyPackageName",
+  },
+
+  mode: "production",
+  target: "web",
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
+
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /(\.ts(x?))|(\.jsx?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              configFile: path.resolve(__dirname, "tsconfig.json"),
+            },
+          },
+        ],
+      },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "source-map-loader",
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          // Use the chain sass-loader -> css-loader -> style-loader
+          // But use MiniCssExtractPlugin on prod, so we get a file.
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [],
+};
